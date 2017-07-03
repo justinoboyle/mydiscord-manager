eba51e5b8d77995df12291c5e3f71bbe58a4847b
Justin O'Boyle <justin@justinoboyle.com>
2017-07-03 01:13:43 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const apiloader = require('mydiscord-api-loader'),
+      _package = require('./package'),
+      fs = require('fs'),
+      path = require('path');
+
+const config = {
+    package: _package,
+    localplugins: path.join(__dirname, 'local-plugins')
+};
+
+if(!fs.existsSync(localplugins))
+    fs.mkdir(localplugins);
+
+apiloader(config);
